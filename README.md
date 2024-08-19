eeabb76e9579227f33accd9c27aed78eff82a3b3
Zelo101 <umadeguy@gmail.com>
2024-08-19 17:31:38 +0100

    Generate normal maps based on LabPBR format

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-Normal_Heights
+Normal_Heights_LabPBR
 ==============
 
-Just a simple (rust) library to create a normal map from a height map.
+Just a simple (rust) library to create a normal LabPBR map from a height map.
+
+See [LabPBR Docs](https://shaderlabs.org/wiki/LabPBR_Material_Standard) for more information.
+
+Material AO (Blue channel) not implemented yet.
 
 Also includes a thin CLI wrapper.
 
@@ -10,14 +14,14 @@ Example Library Usage
 
 ```rust
     let img = image::open(input)?;
-    let normal_map = normal_heights::map_normals(&img);
+    let normal_map = normal_heights_labpbr::map_normals(&img);
     normal_map.save(output)?;
 ```
 
 ```rust
     let img = image::open(input)?;
     let strength = 3.14;
-    let normal_map = normal_heights::map_normals_with_strength(&img, strength);
+    let normal_map = normal_heights_labpbr::map_normals_with_strength(&img, strength);
     normal_map.save(output)?;
 ```
 
@@ -49,13 +53,13 @@ ARGS:
 ### Example Usage
 
 ```sh
-    normal-heights heightmap.png normalmap.png
+    normal-heights-labpr heightmap.png normalmap.png
 ```
 
 Supported Image Formats
 -----------------------
 
-See Image crate, [version 0.23.7](https://github.com/image-rs/image/tree/78568491ed6504c01cdbacbcfc87bd1c5d61fa52#21-supported-image-formats).
+See Image crate, [version ^0.25.2](https://github.com/image-rs/image/tree/main?tab=readme-ov-file#supported-image-formats).
 
 Example Output
 --------------
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
-use clap::{Arg, App, crate_version};
+use clap::{crate_version, App, Arg};
 use std::error::Error;
 use std::str::FromStr;
 
-use normal_heights::{DEFAULT_STRENGTH, map_normals_with_strength};
+use normal_heights_labpbr::{map_normals_with_strength, DEFAULT_STRENGTH};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let default_strength = DEFAULT_STRENGTH.to_string();
-    let matches = App::new("Normal Heights")
+    let matches = App::new("LabPBR Normal Heights")
         .version(crate_version!())
-        .author("Jon Olin")
-        .about("Makes normal maps from height maps.")
-        .arg(Arg::with_name("INPUT")
-            .help("Input height map image file.")
-            .required(true))
-        .arg(Arg::with_name("OUTPUT")
-            .help("Filename to write the normal map to.")
-            .required(true))
-        .arg(Arg::with_name("strength")
-            .help("Strength of the resulting normal map.")
-            .next_line_help(true)
-            .short("s")
-            .long("strength")
-            .default_value(&default_strength)
-            .validator(strength_validator))
+        .author("Zelo101 (fork), Original by Jon Olin")
+        .about("Makes LabPBR normal(_n) maps from height maps.")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input height map image file.")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Filename to write the normal map to.")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("strength")
+                .help("Strength of the resulting normal map.")
+                .next_line_help(true)
+                .short("s")
+                .long("strength")
+                .default_value(&default_strength)
+                .validator(strength_validator),
+        )
         .get_matches();
 
     let input = matches.value_of("INPUT").unwrap();
@@ -31,9 +37,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let img = image::open(input)?;
 
-    let normal_map = map_normals_with_strength(&img, strength);
+    let labpbr_normal_map = map_normals_with_strength(&img, strength);
 
-    normal_map.save(output)?;
+    labpbr_normal_map.save(output)?;
 
     Ok(())
 }
@@ -41,6 +47,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn strength_validator(val: String) -> Result<(), String> {
     match f32::from_str(&val) {
         Ok(_) => Ok(()),
-        Err(_) => Err(format!("'{}' is not a valid number. Please use a base 10 number such as '6' or '3.14'.", val)),
+        Err(_) => Err(format!(
+            "'{}' is not a valid number. Please use a base 10 number such as '6' or '3.14'.",
+            val
+        )),
     }
 }
